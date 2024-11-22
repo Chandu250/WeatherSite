c4f05fff86de30550750a61ec35f554c3086819b
Chandan Bhakare <123458780+Chandu250@users.noreply.github.com>
2024-11-22 17:25:19 +0530

    Final script.js

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ let locationTimezone = document.querySelector(".location-timezone");
 let setIcon = document.querySelector(".icon");
 let maxTemperature = document.querySelector(".maxTemp");
 let minTemperature = document.querySelector(".minTemp");
-let windSpeed = document.querySelector(".windSpeed")
+let windSpeed = document.querySelector(".windSpeed");
 let weather = document.querySelector("#weather");
-
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async position => {
@@ -16,46 +15,37 @@ if (navigator.geolocation) {
         lat = position.coords.latitude;
         const data = await getWeatherdata(lat, long);
 
-    
         // To Draw a India map using leaflet
-        // Map related Code 
         var map = L.map('map').setView([20.9716, 80.5946], 5);
 
         L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=OdpemAaV0raJvYO6cUSS', {
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         }).addTo(map);
 
-
-        // To show a marker on the india map with the name of the place
+        // Show marker with the place name
         var marker = L.marker([lat, long]).addTo(map);
         marker.bindPopup(data.name).openPopup();
-        
 
-        // to add a click handler on map
+        // Add a click handler on the map
         map.on('click', async function(e) {
-            console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng)
+            console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+            const data = await getWeatherdata(e.latlng.lat, e.latlng.lng);
 
-            // Calling the weather api with new lat long
-           const data = await getWeatherdata(e.latlng.lat, e.latlng.lng);
-
-           // Showing the marker at the clicked position with the city name(position name)
+            // Update marker position and popup
             marker.setLatLng([e.latlng.lat, e.latlng.lng]);
             marker.bindPopup(data.name).openPopup();
         });
-        
-
-    })
+    });
 }
 
-    
-async function getWeatherdata(lat,long) {
-        const api = https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=3GXftAhkI0Kp6mm8ehh0;
+async function getWeatherdata(lat, long) {
+    const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=3GXftAhkI0Kp6mm8ehh0`;
 
-        let response = await fetch(api);
-        let data = await response.json();
+    let response = await fetch(api);
+    let data = await response.json();
 
-        weatherDataHandler(data);
-        return data;
+    weatherDataHandler(data);
+    return data;
 }
 
 function weatherDataHandler(data) {
@@ -72,11 +62,10 @@ function weatherDataHandler(data) {
     maxTemperature.textContent = 'Max: ' + temp_max + '\xB0' + ' C';
     minTemperature.textContent = 'Min: ' + temp_min + '\xB0' + ' C';
     windSpeed.textContent = 'Wind Speed: ' + speed + ' m/s';
-    setIcon.style["background-image"] = url(${setIconFunction(icon)});
+    setIcon.style["background-image"] = `url(${setIconFunction(icon)})`;
 }
 
 function setIconFunction(icon) {
-
     const icons = {
         "01d": "./animated/day.svg",
         "02d": "./animated/cloudy-day-1.svg",
