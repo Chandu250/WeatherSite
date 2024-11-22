650de609eb334f5bfd63b8261e196e081760f9a8
Chandan Bhakare <123458780+Chandu250@users.noreply.github.com>
2024-11-22 17:01:54 +0530

    working api script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ if (navigator.geolocation) {
         // Map related Code 
         var map = L.map('map').setView([20.9716, 80.5946], 5);
 
-        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=OdpemAaV0raJvYO6cUSS', {
+        L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=3GXftAhkI0Kp6mm8ehh0', {
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
         }).addTo(map);
 
